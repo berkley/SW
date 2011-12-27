c4eee5d8e2078bb800b840984c8a5889cd72f11d
Chad Berkley <berkley@slickrock.lan>
2011-12-27 14:44:42 -0800

    Initial Commit

---
+++ Spider GPS/SGAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  SGAppDelegate.h
+//  Spider GPS
+//
+//  Created by Chad Berkley on 12/27/11.
+//  Copyright (c) 2011 ucsb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class SGViewController;
+
+@interface SGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) SGViewController *viewController;
+
+@end
---
+++ Spider GPS/SGViewController.h
@@ -0,0 +1,13 @@
+//
+//  SGViewController.h
+//  Spider GPS
+//
+//  Created by Chad Berkley on 12/27/11.
+//  Copyright (c) 2011 ucsb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGViewController : UIViewController
+
+@end

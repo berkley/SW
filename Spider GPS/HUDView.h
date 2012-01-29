fb275a9adcf250f3b134c71f7becea8ff6dc0af2
Chad Berkley <cberkley@gmail.com>
2012-01-29 15:40:35 -0800

    adding eye candy

---
+++ Spider GPS/HUDView.h
@@ -0,0 +1,13 @@
+//
+//  HUDView.h
+//  Spider GPS
+//
+//  Created by Chad Berkley on 1/29/12.
+//  Copyright (c) 2012 ucsb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HUDView : UIView
+
+@end
---
+++ Spider GPS/HUDView.m
@@ -0,0 +1,31 @@
+//
+//  HUDView.m
+//  Spider GPS
+//
+//  Created by Chad Berkley on 1/29/12.
+//  Copyright (c) 2012 ucsb. All rights reserved.
+//
+
+#import "HUDView.h"
+
+@implementation HUDView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
